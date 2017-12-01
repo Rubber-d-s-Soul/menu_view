16ef6fe6f0ade71101454660b7c82fd7cc1e4035
杉山大輔 <rubber.d.s.soul@gmail.com>
2017-12-01 18:08:51 +0900

    レビュー反映

--- D1Menu/js/common.js
+++ D1Menu/js/common.js
@@ -5,4 +5,32 @@ $(function() {
     });
 
 
+    $(conf_dom.center).on("mouseenter", function() {
+        console.log("header hide");
+        $(conf_dom.header).hide("normal");
+        $(conf_dom.class.card_title).hide("normal");
+    });
+
+    $(conf_dom.center).on("mouseleave", function() {
+        console.log("header show");
+        $(conf_dom.header).show("normal");
+        $(conf_dom.class.card_title).show("normal");
+    });
+
+    $('.fixed-action-btn').floatingActionButton({
+        direction: 'top',
+        hoverEnabled: false
+    });
+
+    $(document).ready(function() {
+        $('.tooltipped').tooltip();
+    });
 });
+
+var conf_dom = {
+    header: "header",
+    center: "center",
+    class: {
+        card_title: ".card-title"
+    }
+}
